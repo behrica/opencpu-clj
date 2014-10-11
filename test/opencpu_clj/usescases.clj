de17b89dbeb05c9012afab1ac1ea0ed735d99ecb
Carsten Behring <carsten.behring@gmail.com>
2014-10-12 01:46:28 +0200

    added other exmaple

--- test/opencpu_clj/usescases.clj
+++ test/opencpu_clj/usescases.clj
@@ -1,9 +1,10 @@
 (ns opencpu-clj.usescases
   (:require [midje.sweet :refer [fact]]
             [opencpu-clj.core :refer :all]
-            [clojure.core.matrix.dataset :as ds :refer[dataset]]
-            [clojure.core.matrix :refer [shape]]
-            [opencpu-clj.test-support :refer [server-url j]]))
+            [clojure.core.matrix.dataset :as ds :refer [dataset]]
+            [clojure.core.matrix :refer [shape matrix]]
+            [opencpu-clj.test-support :refer [server-url j]]
+            [clojure.data.json :as json]))
 
 (def mydat
   (let [country ["Angola" "UK" "France"]
@@ -26,6 +27,9 @@
                                            :varying  (j [2,3])
                                            :v.names  (j "gdp")
                                            :direction (j "long")
-                                           }
+                                           }))) => [6 5]))
 
-                                          ))) => [6 5]))
+(fact "can send matrix to R and calculate eigen values with R"
+      (let [m (matrix [[13 2][5 4]])]
+        (call-function-json-RPC server-url "base" "eigen" {:x (json/write-str m)})
+           => {:values [14 3], :vectors [[0.8944 -0.1961] [0.4472 0.9806]]}))
