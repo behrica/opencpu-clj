238ed7e2b2b98f152d9f3f8c82e849e3eef851e6
Carsten Behring <carsten.behring@gmail.com>
2014-10-13 22:08:25 +0200

    new server url

--- test/opencpu_clj/test_support.clj
+++ test/opencpu_clj/test_support.clj
@@ -1,6 +1,6 @@
 (ns opencpu-clj.test-support
   (:require [clojure.data.json :as json :refer[write-str]]))
 
-(def server-url "http://localhost:6124")
+(def server-url "http://localhost:5959")
 
 (def j json/write-str)
